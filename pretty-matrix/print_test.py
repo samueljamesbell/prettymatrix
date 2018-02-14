2ed5818b231b01f1820a5fb6b2801082278d8448
Samuel Bell <samueljamesbell@gmail.com>
2018-02-14 17:08:20 +0000

    Set up for distribution; set up tox

---
+++ pretty-matrix/print_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class PrintTest(unittest.TestCase):
+
+    def test_x(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='pretty-matrix',
+    version='0.0.0',
+    packages=['pretty-matrix',],
+    license='LICENSE',
+    author='Samuel Bell',
+    author_email='samueljamesbell@gmail.com',
+    url='',
+)
