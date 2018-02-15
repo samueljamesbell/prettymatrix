1d75a3e9a41a9d3d95acab624d768d1250ba3813
Samuel Bell <samueljamesbell@gmail.com>
2018-02-15 22:00:51 +0000

    Fix module reference in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='prettymatrix',
     version='0.1.0',
-    py_modules = 'prettymatrix',
+    py_modules = ['prettymatrix'],
     license='MIT',
     author='Samuel Bell',
     author_email='samueljamesbell@gmail.com',
