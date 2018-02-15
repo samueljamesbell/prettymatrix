1b505a5b9ac58824807030b1d4449b54afa77d13
Samuel Bell <samueljamesbell@gmail.com>
2018-02-15 21:48:46 +0000

    Restructure

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='prettymatrix',
-    version='0.0.0',
-    packages=['prettymatrix',],
-    license='LICENSE',
+    version='0.1.0',
+    py_modules = 'prettymatrix',
+    license='MIT',
     author='Samuel Bell',
     author_email='samueljamesbell@gmail.com',
-    url='',
+    url='https://github.com/samueljamesbell/prettymatrix',
+    description='Pretty printer for matrices and column vectors.''',
 )
--- test_prettymatrix.py
+++ test_prettymatrix.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from prettymatrix import prettymatrix
+import prettymatrix
 
 
 class PrettyMatrixTest(unittest.TestCase):
