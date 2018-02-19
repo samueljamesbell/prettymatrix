e2dc0e7d4637687b7cca6dc7a36dad5d6629531c
Samuel Bell <samueljamesbell@gmail.com>
2018-02-19 15:44:55 +0000

    Fix deployment issues; tidy up

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='prettymatrix',
-    version='0.1.0',
-    py_modules = ['prettymatrix'],
+    version='0.1.5',
+    py_modules=['prettymatrix'],
     license='MIT',
     author='Samuel Bell',
     author_email='samueljamesbell@gmail.com',
     url='https://github.com/samueljamesbell/prettymatrix',
-    description='Pretty printer for matrices and column vectors.''',
+    description='Pretty printer for matrices and column vectors.',
+    keywords='matrix matrices vector formatting string numpy',
+    install_requires=['numpy'],
 )
